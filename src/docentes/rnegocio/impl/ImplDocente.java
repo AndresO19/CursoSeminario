19c4a1e5b43f8b009079505132ef25edd3b56343
JhonPC <JhonPC@172.30.58.219>
2018-02-06 17:26:26 -0500

    Docente y testeo

--- src/docentes/rnegocio/impl/ImplDocente.java
+++ src/docentes/rnegocio/impl/ImplDocente.java
@@ -14,10 +14,20 @@ public class ImplDocente implements IDocente {
     @Override
     public int insertar(Docente docente) throws Exception {
         int numFilasAfectadas = 0;
-        String sql = "insert into CursoSeminarios (codigo_c, descripcion) values (?,?)";
+        String sql = "Insert Into Docente (cedula, nombres, apellidos, direcciopn, fechaNacimineto, email, telefono, codDocente) Values (?,?,?,?,?,?,?,?)";
         ArrayList<Parametro> listParametro = new ArrayList<>();
         listParametro.add(new Parametro(1, docente.getCedula()));
-        listParametro.add(new Parametro(2, curso.getDescripcion());
+        listParametro.add(new Parametro(2, docente.getNombres()));
+        listParametro.add(new Parametro(3, docente.getApellidos()));
+        listParametro.add(new Parametro(4, docente.getDirecciopn()));
+        if (docente.getFechaNaciemiento() instanceof java.util.Date) {
+            listParametro.add(new Parametro(5, new java.sql.Date(((java.util.Date) docente.getFechaNaciemiento()).getTime())));
+        } else {
+            listParametro.add(new Parametro(5, docente.getFechaNaciemiento()));
+        }
+        listParametro.add(new Parametro(6, docente.getEmail()));
+        listParametro.add(new Parametro(7, docente.getTelefono()));
+        listParametro.add(new Parametro(8, docente.getCodigoD()));
         Conexion conec = null;
         try {
             conec = new Conexion();
@@ -35,11 +45,21 @@ public class ImplDocente implements IDocente {
     @Override
     public int modificar(Docente docente) throws Exception {
         int numFilasAfectadas = 0;
-        String sql = "UPDATE CursoSeminarios SET codigo_c=?, descripcion=? WHERE codigo_c=?";
+        String sql = "UPDATE Docente SET cedula=?, nombres=?, apellidos=?, direcciopn=?, fechaNacimineto=?, email=?, telefono=?, codDocente=? WHERE codDocente=?";
         ArrayList<Parametro> listParametro = new ArrayList<>();
-        listParametro.add(new Parametro(1, curso.getCodigo()));
-        listParametro.add(new Parametro(2, curso.getDescripcion()));
-        listParametro.add(new Parametro(1, curso.getCodigo()));
+        listParametro.add(new Parametro(1, docente.getCedula()));
+        listParametro.add(new Parametro(2, docente.getNombres()));
+        listParametro.add(new Parametro(3, docente.getApellidos()));
+        listParametro.add(new Parametro(4, docente.getDirecciopn()));
+        if (docente.getFechaNaciemiento() instanceof java.util.Date) {
+            listParametro.add(new Parametro(5, new java.sql.Date(((java.util.Date) docente.getFechaNaciemiento()).getTime())));
+        } else {
+            listParametro.add(new Parametro(5, docente.getFechaNaciemiento()));
+        }
+        listParametro.add(new Parametro(6, docente.getEmail()));
+        listParametro.add(new Parametro(7, docente.getTelefono()));
+        listParametro.add(new Parametro(8, docente.getCodigoD()));
+        listParametro.add(new Parametro(9, docente.getCedula()));
         Conexion conec=null;
         try {
             conec = new Conexion();
@@ -56,9 +76,9 @@ public class ImplDocente implements IDocente {
     @Override
     public int eliminar(Docente docente) throws Exception {
         int numFilasAfectadas = 0;
-        String sql = "DELETE FROM CursoSeminarios WHERE codigo_c=?";
+        String sql = "DELETE FROM Docente WHERE codDocente=?";
         ArrayList<Parametro> listParametro = new ArrayList<>();
-        listParametro.add(new Parametro(1, curso.getCodigo()));
+        listParametro.add(new Parametro(1, docente.getCodigoD()));
         Conexion conec = null;
         try {
             conec = new Conexion();
@@ -75,8 +95,8 @@ public class ImplDocente implements IDocente {
 
     @Override
     public Docente obtener(int codigo) throws Exception {
-        CursoSeminarios curso = null;
-        String sql = "SELECT codigo_c, descripcion FROM CursoSeminarios where codigo_c=?";
+        Docente docente = null;
+        String sql = "Select cedula, nombres, apellidos, direcciopn, fechaNacimineto, email, telefono, codDocente From Docente where codDocente=?";
         ArrayList<Parametro> listadoParametros = new ArrayList<>();
         listadoParametros.add(new Parametro(1, codigo));
         Conexion conec = null;
@@ -85,9 +105,15 @@ public class ImplDocente implements IDocente {
             conec.conectar();
             ResultSet resultado = conec.ejecutarQuery(sql, listadoParametros);
             while (resultado.next()) {
-                curso = new CursoSeminarios();
-                curso.setCodigo(resultado.getInt(1));
-                curso.setDescripcion(resultado.getString(2));
+                docente = new Docente();
+                docente.setCedula(resultado.getString(1));
+                docente.setNombres(resultado.getString(2));
+                docente.setApellidos(resultado.getString(3));
+                docente.setDirecciopn(resultado.getString(4));
+                docente.setFechaNaciemiento(resultado.getDate(5));
+                docente.setEmail(resultado.getString(6));
+                docente.setTelefono(resultado.getString(7));
+                docente.setCodigoD(resultado.getInt(8));
             }
         } catch (Exception e) {
             throw e;
@@ -95,13 +121,13 @@ public class ImplDocente implements IDocente {
             if(conec!=null){
             conec.desconectar();}
         }
-        return curso;
+        return docente;
     }
 
     @Override
     public ArrayList<Docente> obtener() throws Exception {
-        CursoSeminarios curso = null;
-        ArrayList<CursoSeminarios> lista = new ArrayList<>();
+        Docente docente = null;
+        ArrayList<Docente> lista = new ArrayList<>();
         String sql = "SELECT codigo_c, descripcion FROM CursoSeminarios";
         Conexion conec = null;
         try {
@@ -109,10 +135,16 @@ public class ImplDocente implements IDocente {
             conec.conectar();
             ResultSet resultado = conec.ejecutarQuery(sql, null);
             while (resultado.next()) {
-                curso=new CursoSeminarios();
-                curso.setCodigo(resultado.getInt(1));
-                curso.setDescripcion(resultado.getString(2));
-                lista.add(curso);
+                docente = new Docente();
+                docente.setCedula(resultado.getString(1));
+                docente.setNombres(resultado.getString(2));
+                docente.setApellidos(resultado.getString(3));
+                docente.setDirecciopn(resultado.getString(4));
+                docente.setFechaNaciemiento(resultado.getDate(5));
+                docente.setEmail(resultado.getString(6));
+                docente.setTelefono(resultado.getString(7));
+                docente.setCodigoD(resultado.getInt(8));
+                lista.add(docente);
             }
         } catch (Exception e) {
             throw e;
--- test/docente/test/DocenteTest.java
+++ test/docente/test/DocenteTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package docente.test;
 
 import docentes.rnegocio.dao.*;
@@ -14,16 +9,50 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
+import java.util.*;
 
 public class DocenteTest {
-    
+
     public DocenteTest() {
-        
-        
-        
     }
-    
-   
-    
+
+    @Test
+    public void testGeneral() {
+        IDocente docenteDao = new ImplDocente();
+
+// TEST INSERTAR
+        int filas = 0;
+        Docente doc = new Docente("321654987", "andres", "orozco", "Riobamba y la que Cruza",new Date(), "riobamba", "c", 1);
+        try {
+            filas = docenteDao.insertar(doc);
+            System.out.println("Ingreso de " + filas + " Filas Correctas");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        assertTrue(filas > 0);
+//
+//            //TEST OBTENER POR CODIGO
+////        
+        Docente docent = new Docente();
+        try {
+            docent = docenteDao.obtener(1);
+            System.out.println(docent.getCodigoD()+ "    "+docent.getCedula()+ "    "  + docent.getNombres()+ "    " + docent.getApellidos()+ "    " + docent.getDirecciopn()+ "    " + docent.getFechaNaciemiento()+ "    " + docent.getEmail()+ "    " + docent.getTelefono());
+        } catch (Exception e) {
+            System.out.println("error: "+e.getMessage());
+        }
+        assertEquals(docent!=null, true);
+
+//            //TEST LISTADO
+        ArrayList<Docente> docentes = new ArrayList<>();
+        try {
+            docentes = docenteDao.obtener();
+            System.out.println("Docentes");
+            for (Docente docente : docentes) {
+                System.out.println(docent.getCodigoD()+ "    "+docent.getCedula()+ "    "  + docent.getNombres()+ "    " + docent.getApellidos()+ "    " + docent.getDirecciopn()+ "    " + docent.getFechaNaciemiento()+ "    " + docent.getEmail()+ "    " + docent.getTelefono());
+            }
+        } catch (Exception e) {
+        }
+        assertTrue(docentes.size()>0);
+    }
+
 }
